a113659f7f4a07f1db2630e6ddcabdc30c1a4861
Kate Currie <katecurrie@kismet.local>
2009-03-21 21:11:47 +1000

    added styles for external tools list, fixed a bit of js

--- static/admin/js/general.js
+++ static/admin/js/general.js
@@ -13,15 +13,13 @@ function SplitFile()
 }
 
 dojo.addOnLoad(function() {
-    dojo.query("topsplash").innerHTML = document.domain;
+    dojo.byId("topsplash").innerHTML = document.domain;
 
     // get url minus "http://"
     url = document.URL.substring(7, document.URL.length);
     var currentPage = url.substring(url.indexOf("/"), url.length);
     
-    var menuTabs = dojo.query("menu").childNodes;
-    
-    if (!menuTabs) return;
+    var menuTabs = dojo.byId("menu").childNodes;
     
     for (i = 0; i < menuTabs.length; i++) {
     	if (menuTabs[i].nodeName == "LI") {
